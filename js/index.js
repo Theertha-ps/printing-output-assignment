23548d9ad7a7f7878a0bce0cc913017b9a86b931
Theertha-ps <pull0036@algonquinlive.com>
2025-05-13 14:19:41 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,88 @@
+// task 1
+
+// alert( "description of alerted_value: " + alerted_value );
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+
+let a = 3;
+let b = 5;
+let c = undefined
+
+let output = `
+let a = 3;
+let b = 5;
+let c;
+-----------
+
+a + b = ${a+b}
+a - b = ${a-b}
+a * b = ${a*b}
+a / b = ${a/b}
+a % b = ${a%b} 
+a += b = ${a += b}  
+a -= b = ${a -= b }
+a *= b = ${a *= b }
+a /= b = ${a /= b }
+a %= b = ${a %= b }
+a == b = ${a == b }
+a != b = ${a != b }
+a > b = ${a > b }
+a < b = ${a < b }
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c}
+
+`
+console.log(output);
+
+// task 2
+
+let firstname = "Theertha";
+
+let lastname = "Pullanikkal Shaji";
+
+let email = "pull0036@algonquinlive";
+
+let output1;
+
+output1 = `My name is ${firstname} ${lastname}. You can contact me at ${email}.`; 
+
+console.log(output1);
+alert(output1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
